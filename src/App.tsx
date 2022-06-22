e5e5dd9d42ec4bb9617a50b792015afb0a22fdbd
Siyn Ma <mrtatekii33@gmail.com>
2022-06-22 11:45:54 +0800

    添加样式文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function App() {
-  return <div className="App">App</div>;
+  return(
+    <>
+      <h1>Hello world</h1>
+      <h2>Hello world</h2>
+      <h3>Hello world</h3>
+      <code>const a = '123'</code>
+    </>
+  )
 }
 
 export default App;
